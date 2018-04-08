5643fd30d697c8f682da19aa718305ae8ef09ac7
Ahmed Salem <ahmedsalem2008@gmail.com>
2018-04-08 16:40:05 +0300

    Push to github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MembersComponent } from './pages/members/members.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { UploadComponent } from './pages/upload/upload.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    MembersComponent,
+    RegisterComponent,
+    LoginComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule
